352e973c0a52d71fceac9ec4af91f6ef6549fc38
Ross Daniel Kiser <kiserr10@gmail.com>
2018-05-01 15:30:03 -0600

    game config in palce

---
+++ app.js
@@ -0,0 +1,47 @@
+const config = {
+    type: Phaser.AUTO, //this is the rendering context, auto tries to use WebGL fallback is canvas
+    width: 800, //game resolution
+    height: 600,//game resolution
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    }, 
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+let game = new Phaser.Game(config);
+
+//initialize variables//
+
+let player;
+let starfield;
+
+//gaming functions//
+
+function preload(){
+    this.load.image('starfield', './assets/starfield.png');
+    this.load.image('spaceship', './assets/player.png');
+}
+
+function create(){
+    //scrolling background//
+    starfield = game;
+    //the hero//
+    player = game.add.sprite(400, 500, 'spaceship');
+    player.anchor.setTo(0.5, 0.5);
+}
+
+function update(){
+    //Scroll the background//
+    starfield.tilePosition.y =+ 2;
+}
+function render(){
+
+}
